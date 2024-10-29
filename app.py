12abdc5691564773099e4eac37f37ad74f28ae21
elon-shmusk <brdemyan@gmail.com>
2024-10-29 18:37:55 -0400

    updated search function (working)

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     query = request.args.get('query')  # Get the query from the search form
-    google_books_api_key = os.getenv('GOOGLE_BOOKS_API_KEY')  # Get API key from environment variable
+    google_books_api_key = os.getenv('API_KEY')  # Get API key from environment variable
     google_books_api = f"https://www.googleapis.com/books/v1/volumes?q={query}&key={google_books_api_key}"
     
     response = requests.get(google_books_api)
@@ -25,4 +25,4 @@ def search():
     return jsonify(data)  # Return Google Books API data as JSON to frontend
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
