f0e2867f5963b7259faf616ac2f24e78a9d670d9
cadizsd <143456301+cadizsd@users.noreply.github.com>
2024-11-01 11:33:42 -0400

    fixes issues with userdata.sh
    fixes the userdata issue and allows it to be used in the ec2 userdata

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+# Install Git
 sudo yum install -y git
-sudo git clone https://github.com/cadizsd/reading_test.git
-cd reading_test
+
+# Clone the repository
+git clone https://github.com/cadizsd/reading_test.git /home/ec2-user/reading_test
+
+# Move to the repository directory
+cd /home/ec2-user/reading_test || exit
+
+# Set up the Python virtual environment
 sudo python3 -m venv .venv
-sudo .venv/bin/pip install -r requirements.txt
+source .venv/bin/activate
+
+# Install required dependencies
+.venv/bin/pip install -r requirements.txt
+
+# Copy the service file and enable the service
 sudo cp reading.service /etc/systemd/system
+sudo systemctl daemon-reload
 sudo systemctl enable reading
 sudo systemctl start reading
